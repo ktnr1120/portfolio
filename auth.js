54b7d8e983905f8b7d123ab14d13629411dcc366
ktnr1120 <141934126+ktnr1120@users.noreply.github.com>
2023-10-26 00:43:58 +0900

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,14 +1,10 @@
 const router = require("express").Router();
-const { query, body, validationResult } = require('express-validator');
-//const { User } = require("../DB/user");
+const { body, validationResult } = require('express-validator');
 const mysql = require("mysql");
 const bcrypt = require("bcrypt");
 const JWT = require("jsonwebtoken");
-const auth = require("dotenv");
-router.get("/", (req,res) => {
-    res.send("Hello Authjs");
-});
 
+//データベースの設定
 const dbConfig = {
     connectionLimit: 10,
     host: "localhost",
@@ -17,184 +13,107 @@ const dbConfig = {
     database: "memory",
 };
 
+//データベースのonoff
 const pool = mysql.createPool(dbConfig);
 
-router.post("/register", async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    // パスワードのハッシュ化
-    const hashedPassword = await bcrypt.hash(password, 10);
+//ユーザー新規登録
+router.post(
+    "/register",
+    body("email").isEmail(),
+    body("password").isLength({ min: 6 }),
+    async (req, res) => {
+        const errors = validationResult(req);
 
-router.post("/login", async (req,res) => {
-    const { email, password } = req.body;
-
-    //DBからユーザー情報を取得
-    pool.query("SELECT * FROM users WHERE email = ?", [email], async (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: "データベースクエリエラー"});
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
 
-        if (result.length === 0) {
-            return res.status(400).json({ error: "ユーザーが存在しません" });
-        }
+        const email = req.body.email;
+        const password = req.body.password;
 
-        const user = result[0];
-        const isMatch = await bcrypt.conpara(password, user.password_hash);
-
-        if(!isMatch) {
-            return res.status(400).json({ error: "パスワードが異なります"});
-        }
-    });
-});
-});
-
-//     // データベースにユーザーを挿入(仮DB用)
-//     pool.getConnection((err, connection) => {
-//         if (err) {
-//             console.error("データベース接続エラー: " + err.message);
-//             return res.status(500).send("データベース接続エラー");
-//         }
-
-//         connection.query("INSERT INTO users (email, password) VALUES (?, ?)", [email, hashedPassword], (err, result) => {
-//             connection.release();
-//             if (err) {
-//                 console.error("データベースクエリエラー: " + err.message);
-//                 return res.status(500).send("データベースクエリエラー");
-//             }
-
-//             // ユーザー登録成功の応答
-//             res.json({ message: "ユーザー登録が成功しました" });
-//         });
-//     });
-// });
-
-router.post("/login", async (req, res) => {
-    const { email, password } =req.body;
-
-    //データベースからユーザーを取得
-    pool.getConnection((error, connection) => {
-        if(err) {
-            console.error("データベース接続エラー: " + err.message);
-            return res.status(500).send("データベース接続エラー");
-        }
-
-        connection.query("SELECT * FROM users WHERE email = ?", [email], async (err, rows) => {
-            connection.release();
-
-            if(err) {
-                console.error("データベースクエリエラー:  "+ err.message);
+        // ユーザーの存在確認
+        pool.query("SELECT * FROM users WHERE email = ?", [email], (err, rows) => {
+            if (err) {
+                console.error("データベースクエリエラー :" + err.message);
                 return res.status(500).send("データベースクエリエラー");
             }
-            
-            if (rows.length === 0) {
-                return res.status(400).json({ message: "ユーザーが存在しません"});
+
+            if (rows.length > 0) {
+                return res.status(400).json({
+                    message: "すでにユーザーは存在しています",
+                });
+            } else {
+                // パスワードの暗号化
+                bcrypt.hash(password, 10, (err, hashedPassword) => {
+                    if (err) {
+                        console.error("パスワードの暗号化エラー: " + err.message);
+                        return res.status(500).send("パスワードの暗号化エラー");
+                    }
+
+                    // ユーザーをDBに保存
+                    pool.query(
+                        "INSERT INTO users (email, password) VALUES (?, ?)",
+                        [email, hashedPassword],
+                        (err, result) => {
+                            if (err) {
+                                console.error("データベースクエリエラー: " + err.message);
+                                return res.status(500).send("データベースクエリエラー");
+                            }
+
+                            // JWTの発行とクライアントへのレスポンス
+                            const token = JWT.sign(
+                                {
+                                    email,
+                                },
+                                process.env.SECRETKEY, // ここでSECRETKEYを設定している必要があります
+                                {
+                                    expiresIn: "24h",
+                                }
+                            );
+                            res.json({ token });
+                        }
+                    );
+                });
             }
-
-            const user = rows[0];
-            const isMatch = await bcrypt.compare(password, user.password);
-
-            if(!isMatch) {
-                return res.status(400).json({ message: "パスワードが異なります"});
-            }
-
-            //ログイン成功時の応答とトークンの発行
-            const token = JWT.sign({ email }, process.env.SECRETKEY, {expiresIn: "24h" });
-            res.json({ token });
         });
+    }
+);
+
+//ログイン用のAPI
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    pool.query("SELECT * FROM users WHERE email = ?", [email], async (err, rows) => {
+        if (err) {
+            console.error("データベースクエリエラー: " + err.message);
+            return res.status(500).send("データベースクエリエラー");
+        }
+
+        if (rows.length === 0) {
+            return res.status(400).json({ message: "ユーザーが存在しません" });
+        }
+
+        const user = rows[0];
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "パスワードが異なります" });
+        }
+
+        //ログイン成功時にトークンの発行
+        const token = JWT.sign(
+            { email },
+            process.env.SECRETKEY,
+            { expiresIn: "24h" }
+        );
+        res.json({ token });
     });
 });
 
-// //ユーザー新規登録
-// router.post(
-//     "/register", 
-//     //バリデーションチェック
-//     body("email").isEmail(),
-//     body("password").isLength({min: 6 }),
-//     async (req, res) => {
-//     const email = req.body.email;
-//     const password = req.body.password;
+router.get("/allUsers", (req, res) => {
+    // ユーザー情報を取得するための処理が不足
+    // このエンドポイントの処理を追加する
+    res.status(501).send("Not Implemented");
+});
 
-//     const erros = validationResult(req);
-//     if(!erros.isEmpty()) {
-//         return res.status(400).json({ erros: erros.array() });
-//     }
-
-//     //DBにユーザーが存在しているか確認
-//     const user = User.find((user) => user.email === email);
-//     if(user) {
-//         return res.status(400).json([
-//             {
-//                 massage: "すでにそのユーザーは存在しています。",
-//             },
-//         ]);
-//     }
-
-//     //パスワードの暗号化
-//     let hashedPassword = await bcrypt.hash(password, 10)
-//     //console.log(hashedPassword);
-
-//     //DBへの保存
-//     User.push({
-//         email,
-//         password: hashedPassword,
-//     });
-
-//     //クライアントへJWTの発行
-//     const token = await JWT.sign(
-//         {
-//             email,
-//         },
-//         process.env.SECRETKEY,//これは直書きしない、見えないファイルを設定する
-//         {
-//             expiresIn: "24h",
-//         }
-//     );
-
-//     return res.json({
-//         token: token,
-//     });
-// });
-
-// //ログイン用のAPI
-// router.post("/login", async(req, res) => {
-//     const { email, password } = req.body;
-    
-//     const user = User.find((user) => user.email === email);
-//     if(!user) {
-//         return res.status(400).json([
-//             {
-//                 message: "ユーザーが存在しません。",
-//             },
-//         ]);
-//     }
-
-//     //パスワードの複合、照合
-//     const isMatch = await bcrypt.compare(password, user.password);
-
-//     if(!isMatch) {
-//         return res.status(400).json([
-//             {
-//                 massage: "パスワードが異なります",
-//             },
-//         ]);
-//     }
-//     const token = await JWT.sign(
-//         {
-//             email,
-//         },
-//         process.env.SECRETKEY,//これは直書きしない、見えないファイルを設定する
-//         {
-//             expiresIn: "24h",
-//         }
-//     );
-
-//     return res.json({
-//         token: token,
-//     });
-// });
-
-// //DBのユーザーを確認するAPI
-// router.get("/allUsers", (req, res) => {
-//     return res.json(User);
-// })
 module.exports = router;
